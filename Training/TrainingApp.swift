ad8b80475e4590ebb2224303c599337cb4f033a9
Brunods10 <brunods1001@gmail.com>
2023-09-04 06:04:52 -0400

    Initial Commit

---
+++ Training/TrainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrainingApp.swift
+//  Training
+//
+//  Created by Bruno dos Santos on 9/4/23.
+//
+
+import SwiftUI
+
+@main
+struct TrainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
